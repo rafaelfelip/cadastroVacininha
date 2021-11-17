d67d42cddc1596fc363e185bca45cc2ccb0f4066
Rafael felip <rafael.felip4@gmail.com>
2021-11-16 21:14:47 -0300

    16.11.21

--- main.c
+++ main.c
@@ -11,21 +11,13 @@ typedef struct {
 // variaveis globais
 int numero;
 
-char nome[50];
-char cpf[50];
-char idade[50];
-char sexo[50];
-char endereco[50];
-char cidade[50];
-char estado[50];
-
 
 /******************* função principal (main) *********************/
 int main() {
 
     login();
     menu();
-    cadastro();    
+    cadastrar_paciente();   
 
     return 0;
 }
@@ -73,12 +65,12 @@ void menu(){
     printf("Selecione a opção desejada: \n");
     scanf("%d", &numero);
     switch (numero) {
-        case 1:consulta();
+        case 1:        
 
         break;
 
-        case 2:cadastro();
-
+        case 2:
+            cadastrar_paciente();
         break;
     }
 
@@ -86,40 +78,134 @@ void menu(){
 }
 
 /******************* função CADASTRO *********************/
-void cadastro() {
+void cadastrar_paciente(){   
 
+    typedef struct {
+        char nome[50];
+        char cpf[14]; 
+        char telefone[13]; 
+        char endereco[50];
+        char rua[50];
+        char numero[8];
+        char bairro[50];
+        char cidade[50];
+        char estado[2];
+        char cep[11]; 
+        char dt_nascimento[10];
+        char email[50];
+        char dt_diagnostico[10];
+        char diabetes[1];
+        char obesidade[1];
+        char hipertensao[1];
+        char tuberculose[1];
+        char outros[1];
+    }
+  
+    paciente; paciente pac[100];
+    int i = 0; 
+ 
+    printf("\t\t\tCADASTRO_DE_PACIENTE\n\n\n");     
+     
+    printf("Nome: "); 
+    getchar(); 
+    gets(pac[i].nome);    
+            
+    printf("CPF 000.000.000-00): ");
+    gets(pac[i].cpf);       
 
-    printf("\t\t\t*****CADASTRO DE PACIENTE*****\n\n");
+    printf("Telefone  00-0000-0000): ");
+    gets(pac[i].telefone);       
+     
+    printf("Endereco Rua: ");
+    gets(pac[i].rua);       
+     
+    printf("Nº Residencial: ");
+    gets(pac[i].numero);       
+ 
+    printf("Bairro: ");
+    gets(pac[i].bairro);       
     
-    printf("\nNome: ");
-    scanf("%d", &nome);
-    gets(nome);
+    printf("Cidade: ");
+    gets(pac[i].cidade);       
 
-    printf("\nCPF: ");
-    scanf("%d", &cpf);
-    gets(cpf);
+    printf("UF do Estado: ");
+    gets(pac[i].estado);       
+    
+    printf("Digite o CEP 00.000-000: ");
+    gets(pac[i].cep);       
 
-    printf("\nIdade: ");
-    scanf("%d", &idade);
-    gets(idade);
+    printf("Data de Nascimento dd/mm/aaaa: ");
+    gets(pac[i].dt_nascimento);       
+    
+    printf("E-mail: ");
+    gets(pac[i].email);       
 
-    printf("\nSexo: ");
-    scanf("%d", &sexo);
-    gets(sexo);
+    printf("Data Diagnostico (dd/mm/aaaa): ");
+    gets(pac[i].dt_diagnostico);    
 
-    printf("\nEndereço: ");
-    scanf("%d", &endereco);
-    gets(endereco);
+    printf("Possui diabetes? (S ou N): ");
+    gets(pac[i].diabetes); 
+    
+    printf("Possui obesidade? (S ou N): ");
+    gets(pac[i].obesidade); 
 
-    printf("\nCidade: ");
-    scanf("%d", &cidade);
-    gets(endereco);
+    printf("Possui hipertensao? (S ou N): ");
+    gets(pac[i].hipertensao); 
 
-    printf("\nEstado: ");
-    scanf("%d", &estado);
-    gets(endereco);
+    printf("Possui tuberculose? (S ou N): ");
+    gets(pac[i].tuberculose); 
 
-    printf("\n\ncadastro concluido, Paciente nome, portador do CPF: cpf, com idade anos de idade, do sexo: sexo residente am")
+    printf("Possui outras comodidades? (S ou N): ");
+    gets(pac[i].outros); 
+
+    i++;    
+
+    int v;
+	int len_vet = sizeof(paciente); // tamanho do vetor
+	FILE * arq; 
+    arq = fopen("dados.bin", "ab");
+
+    if(arq != NULL){
+		int indice = 0;
+		while(1){
+			paciente p; 
+			size_t r = fread(&p, sizeof(paciente), 1, arq);
+
+			// se retorno for menor que o count, então sai do loop
+			if(r < 1)
+				break;
+			else
+				aux_pacientes[indice++] = p;
+		}
+		fclose(arq); // fecha o arquivo
+		return indice;
+	}else{
+		printf("\nErro ao abrir o arquivo para leitura!\n");
+		exit(1); // aborta o programa
+	}
+ 
+    system("clear");   
+    printf("\t\t\t CADASTRO DE PACIENTE \n\n"); 
+    printf("1 - Voltar Menu Principal. \n");
+    printf("2 - Cadastrar Novo paciente. \n");    
+    printf("3 - Sair. \n\n");    
+    printf("Selecione a opção desejada: \n");
+   
+    scanf("%d", &numero);   
+    switch (numero) {
+        case 1:
+            menu();
+        break;
+
+        case 2:
+            cadastrar_paciente();
+        break;
+
+        case 3:
+            system("clear");  
+            printf("\n\nBYE :).\n\n");  
+            break;
+    }
 }
 
 /******************* função AGENDAMENTO *********************/
